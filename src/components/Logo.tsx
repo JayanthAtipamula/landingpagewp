1b57b3b3b5e30ea1d4bb93b2693b24f31ff4fb80
avinash gudla <jayanthnetha9347@gmail.com>
2024-12-11 09:09:31 +0000

    2nd commit

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export function Logo() {
   return (
     <div className="flex flex-col items-center">
-      <div className="w-32 h-32 flex items-center justify-center">
+      <div className="w-40 h-40 flex items-center justify-center">
         <img
           src="https://agxfactor.netlify.app/assets/MARKETING__4_-removebg-preview%20(1)-cc2cfa98.png"
           alt="AGX Factor Logo"
